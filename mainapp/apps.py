875c66aed8db6c08370c8f50a7b6b6763fa573b5
venkat66 <venkatashivaji979@gmail.com>
2023-09-07 19:05:47 +0530

    bot check

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+import threading
+import os
 
 class MainappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mainapp"
+
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
 from dotenv import load_dotenv
 
 
@@ -17,8 +18,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # Run the Discord bot script as a separate process
+
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
+    subprocess.Popen(["python", "discord_bot.py"])
     main()
