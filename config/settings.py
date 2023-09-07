1176dd34ce15b5695677aaad928a7da22823b395
venkat66 <venkatashivaji979@gmail.com>
2023-09-07 18:06:52 +0530

    csrf_issue_fix3

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ["codestego.up.railway.app",'127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['codestego.up.railway.app','127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['codestego.up.railway.app','127.0.0.1']
 
 
 # Application definition
