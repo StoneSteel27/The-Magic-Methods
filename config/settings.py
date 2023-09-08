bbb84072ed8e0f042ac345749baafed567682e49
Rohan Sharma <96976448+ilovetensor@users.noreply.github.com>
2023-09-08 11:43:44 +0530

    Update settings.py err fix ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["codestego.up.railway.app",'127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://codestego.up.railway.app', 'codestego.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://codestego.up.railway.app', 'https://codestego.onrender.com/']
 
 
 # Application definition
