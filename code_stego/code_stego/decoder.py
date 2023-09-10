b9d1562e348da6484d959d6045de7e4eb928e461
kanishq vijay <stonesteel27@gmail.com>
2023-09-10 20:57:47 +0530

    Documenting encoder and decoder

--- code_stego/code_stego/decoder.py
+++ code_stego/code_stego/decoder.py
@@ -1,6 +1,4 @@
-"""
-Decode an image built by encoder.encode
-"""
+"""Implements decoder function"""
 
 import numpy as np
 import PIL.Image
@@ -11,27 +9,38 @@ from .constants import (
 
 
 def decode(image: PIL.Image.Image):
-    """
-    Extract the text from an image built by encoder.encode
-    """
-    im_arr = np.array(image)
-    flat = im_arr.flatten()
-    chunks = np.bitwise_and(flat, USED_BITS_MASK)
-    magic_test = 0
+    """Extracts the text from encoded image"""
+    # loads the image and retrieves encoded chunks from the image
+    image_array = np.array(image)
+    flattened_array = image_array.flatten()
+    chunks = np.bitwise_and(flattened_array, USED_BITS_MASK)
+
+    # tests for checking if the stored data is damaged
+    magic_number_test = 0
     for i, off in zip(range(4), range(12, -1, -4)):
-        magic_test |= chunks[i] << off
-    if magic_test != MAGIC_NUMBER:
+        magic_number_test |= chunks[i] << off
+    if magic_number_test != MAGIC_NUMBER:
         raise ValueError("magic number does not match")
-    code_len = 0
+
+    # reads stored header data
+    code_length = 0
     for i, off in zip(range(4, 12), range(28, -1, -4)):
-        code_len |= chunks[i] << off
-    chunks_per_char = 8 // USED_BITS_PER_BYTE
-    num_code_chunks = code_len * chunks_per_char
-    code_chunks = chunks[HEADER_SIZE : HEADER_SIZE + num_code_chunks]
+        code_length |= chunks[i] << off
+
+    # removing header data
+    chunks_per_character = 8 // USED_BITS_PER_BYTE
+    num_code_chunks = code_length * chunks_per_character
+    code_chunks = chunks[HEADER_SIZE:HEADER_SIZE + num_code_chunks]
+
+    # convert chunk array to bytes of data
     code_chars = []
-    for i in range(0, len(code_chunks), chunks_per_char):
-        char = 0
-        for char_chunk in code_chunks[i : i + chunks_per_char]:
-            char = (char << USED_BITS_PER_BYTE) | char_chunk
-        code_chars.append(char)
-    return bytes(code_chars).decode("ascii")
+    for i in range(0, len(code_chunks), chunks_per_character):
+        character = 0
+        for character_chunk in code_chunks[i:i + chunks_per_character]:
+            character = (character << USED_BITS_PER_BYTE) | character_chunk
+        code_chars.append(character)
+
+    # convert bytes to string
+    decoded_text = bytes(code_chars).decode("ascii")
+
+    return decoded_text
